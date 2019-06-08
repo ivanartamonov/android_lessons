cd150805be76e1004432ab0357d7b729f3856af6
Ivan Artamonov <i.o.artamonov@gmail.com>
2019-06-08 19:33:26 +0300

    Setup retrofit and fetch User by API

--- app/src/main/java/com/artamonov/lessons/MainActivity.java
+++ app/src/main/java/com/artamonov/lessons/MainActivity.java
@@ -1,5 +1,6 @@
 package com.artamonov.lessons;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -9,13 +10,18 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.artamonov.lessons.models.User;
+import com.artamonov.lessons.network.NetworkService;
 import com.artamonov.lessons.ui.home.WorkoutDetailFragment;
 import com.artamonov.lessons.ui.home.WorkoutHistoryAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements WorkoutHistoryAdapter.IDetailWorkoutListener {
 
@@ -38,6 +44,40 @@ public class MainActivity extends AppCompatActivity implements WorkoutHistoryAda
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        this.setUser();
+    }
+
+    private void setUser() {
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getUser()
+                .enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
+                        User user = response.body();
+
+                        if (user != null) {
+                            Log.d("RETR_API", user.getName());
+                            TextView tvUserName = findViewById(R.id.tv_user_name);
+                            tvUserName.setText(user.getName());
+                        }
+
+                        /*
+                        textView.append(post.getId() + "\n");
+                        textView.append(post.getUserId() + "\n");
+                        textView.append(post.getTitle() + "\n");
+                        textView.append(post.getBody() + "\n");
+                        */
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<User> call, @NonNull Throwable t) {
+                        //Toast.makeText(this, "Error occurred while getting request!", Toast.LENGTH_LONG).show();
+                        //textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
     }
 
     @Override
