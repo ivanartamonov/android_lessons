12a134ec1047d6a54db451816e0ba58a3eafff23
Ivan Artamonov <i.o.artamonov@gmail.com>
2019-06-12 16:50:33 +0300

    Comment problems

--- app/src/main/java/com/artamonov/lessons/services/InitUser.java
+++ app/src/main/java/com/artamonov/lessons/services/InitUser.java
@@ -23,6 +23,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+/**
+ * Задача такая: при запуске приложения, нужно "залогинить" пользоватея.
+ * Самой процедуры логина нет и пока не будет, мне нужно просто хардкорно
+ * взять юезара по ID из АПИ и залогинить его.
+ *
+ * "Залогинить" - имеется ввиду, что в дровере будет его имя и аватарка.
+ *
+ * Что я пытаюсь селать:
+ * 1) Проверить, есть ли юзер в локальной базе
+ * 2) Если есть, то достаю его оттуда и "логиню". Т.е. пишу в дровере его имя и ставлю аватарку
+ * Если в базе нету, то:
+ * 3) Получаю его по API, "логиню" и попутно сохраняю в локальную базу на будущее
+ *
+ * Проблемы:
+ * - почему-то нельзя обратиться к activity внутри Consumer класса ниже (см.комменты далее)
+ * - ощущение, что тут вообще всё неправильно... Как можно более лаконично и "читаемо" делать вызовы к API и базе?
+ * За всей этой шелухой и бесконечными обертками трудно прочитать что вообще происходит.
+ */
 public class InitUser {
 
     private static final String TAG = "InitUser";
@@ -41,7 +59,8 @@ public class InitUser {
         }
 
         loadUser();
-        //fetchFromApi();
+
+        //fetchFromApi(); // Этот метод работает
 
         return this.user;
     }
@@ -60,12 +79,18 @@ public class InitUser {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
 
+        // Как всю эту херню написать нормально? Весь этот асинхронный rx вызов?
+        // Это же какая-то жесть...
         observable.subscribe(new Consumer<User>() {
             @Override
             public void accept(final User dbUser) throws Exception {
-                Log.d(TAG, "Fetched from DB: " + dbUser.getName());
+                Log.d(TAG, "Fetched from DB: " + dbUser.getName()); // Работает, выводит имя
                 if (dbUser != null) {
-                    SetUserInfo.execute(activity, dbUser);
+                    // При попытке обратиться к activity отсюда, приложение вылетает.
+                    // Log.d(TAG, activity.getCallingActivity().toString()); // КРАШ...
+
+                    // SetUserInfo должен установит имя и аватарку в дровере
+                    // SetUserInfo.execute(activity, dbUser);
                 } else {
                     //fetchFromApi();
                 }
@@ -104,6 +129,10 @@ public class InitUser {
 
     private void saveUserInDatabase(User user) {
         Log.d(TAG, "Saving user in database...");
+
+        // Снова какая-то жесть... мне нужно вызвать 2 строчки кода, сохраняющие юзера в базу...
+        // А для этого надо написать 25 строчек с двумя анонимными классами...
+        // Как это делать правильно?
 
         class SaveInDatabaseAction implements Callable<User> {
 
