53aaf100a1af25501aade539c49830f6d4c87f92
Ivan Artamonov <i.o.artamonov@gmail.com>
2019-07-06 13:39:23 +0300

    Fix workout history list

--- app/src/main/java/online/yourfit/data/workout_history/WorkoutHistoryManager.java
+++ app/src/main/java/online/yourfit/data/workout_history/WorkoutHistoryManager.java
@@ -1,7 +1,5 @@
 package online.yourfit.data.workout_history;
 
-import online.yourfit.data.workout_history.WorkoutHistoryItem;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,32 +10,11 @@ public class WorkoutHistoryManager {
     private static List<WorkoutHistoryItem> workoutHistoryItems = new ArrayList<>();
 
     public static List<WorkoutHistoryItem> getList() {
-        workoutHistoryItems.add(new WorkoutHistoryItem("02.05", "My program", 3600, 5400));
-        workoutHistoryItems.add(new WorkoutHistoryItem("03.05", "My program", 3700, 5200));
-        workoutHistoryItems.add(new WorkoutHistoryItem("04.05", "My program", 3800, 7300));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-
-        /*
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        workoutHistoryItems.add(new WorkoutHistoryItem("05.05", "My program", 3900, 6420));
-        */
+        if (workoutHistoryItems.size() == 0) {
+            workoutHistoryItems.add(new WorkoutHistoryItem("02.05", "My program", 3600, 5400));
+            workoutHistoryItems.add(new WorkoutHistoryItem("03.05", "My program", 3700, 5200));
+            workoutHistoryItems.add(new WorkoutHistoryItem("04.05", "My program", 3800, 7300));
+        }
 
         return workoutHistoryItems;
     }
