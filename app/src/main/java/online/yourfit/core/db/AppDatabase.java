e9e0d27d7be3d1454acc4d7d434daf08167a6376
Ivan Artamonov <i.o.artamonov@gmail.com>
2019-07-21 12:42:30 +0300

    Refactor AppDatabase after di setup

--- app/src/main/java/online/yourfit/core/db/AppDatabase.java
+++ app/src/main/java/online/yourfit/core/db/AppDatabase.java
@@ -1,9 +1,6 @@
 package online.yourfit.core.db;
 
-import android.content.Context;
-
 import androidx.room.Database;
-import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import online.yourfit.data.exercises.Exercise;
@@ -15,17 +12,6 @@ import online.yourfit.data.user.User;
 
 @Database(entities = {User.class, Program.class, Exercise.class}, version = 4)
 public abstract class AppDatabase extends RoomDatabase {
-
-    private static AppDatabase instance;
-
-    public static synchronized AppDatabase getInstance(Context context) {
-        if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "database")
-                    .fallbackToDestructiveMigration()
-                    .build();
-        }
-        return instance;
-    }
 
     public abstract UserDao userDao();
 
